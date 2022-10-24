9fc606d33435bfe64a058bc99b24e5408205b652
Ron <ronald.tucuman@outlook.com>
2022-10-24 04:43:17 -0400

    added DOMAIN_URL to avoid CSRF verification failed

--- condomanager/settings.py
+++ condomanager/settings.py
@@ -135,3 +135,5 @@ MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DOMAIN_URL = os.environ['DOMAIN_URL']
