877f3dfc0d523e069aa10cd9e52b02011dfef742
Ron <ronald.tucuman@outlook.com>
2022-12-15 00:14:23 -0400

    fixed url path issue

--- area_comun/urls.py
+++ area_comun/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import registrar_area_comun
 
 urlpatterns = [
-    path('/registrar', registrar_area_comun, name='registrar_area_comun'),
+    path('registrar/', registrar_area_comun, name='registrar_area_comun'),
 ]
