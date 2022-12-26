5bdeb6714fd883d5ea0f22e77b058657a0f34e4a
rontucuman <ronald.tucuman@outlook.com>
2022-12-26 04:19:35 -0400

    confirm and cancel part 1

---
+++ receipt/Reservation.py
@@ -0,0 +1,18 @@
+import datetime
+
+
+class Reservation:
+    def __init__(self):
+        self.reservation_id = 0
+        self.receipt_id = 0
+        self.user_id = 0
+        self.username = ''
+        self.common_area_id = 0
+        self.common_area_name = ''
+        self.reservation_date = datetime.datetime.now()
+        self.begin_reservation_date = datetime.datetime.now()
+        self.end_reservation_date = datetime.datetime.now()
+        self.amount = 0
+        self.is_confirmed = False
+        self.is_canceled = False
+
