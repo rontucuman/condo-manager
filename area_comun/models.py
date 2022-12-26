97001054a65babb0b5f303e2a9c1f2366fff5af7
Jean Carlo Rodriguez <ajenadrojeancarlo@gmail.com>
2022-12-26 17:38:31 -0400

    Agregando lista de areas comunes, reserva de areas comunes y mis reservas

--- area_comun/models.py
+++ area_comun/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
 # Create your models here.
 
@@ -7,6 +10,20 @@ class AreaComun(models.Model):
     descripcion = models.TextField(max_length=100)
     mobiliario = models.TextField(max_length=100, default='Ninguno')
     costo = models.PositiveIntegerField(default=0)
+    # Actualmente solo soportamos una cuenta de administrador entonces no necesitamos la siguiente relacion por el momento
+    # administrador = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"id:{self.id} nombre:{self.nombre}"
+
+class ReservaAreaComun(models.Model):
+    area_comun = models.ForeignKey(AreaComun, on_delete=models.CASCADE, related_name="disponibilidadDeAreaComun")
+    propietario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="misReservas")
+    fecha = models.DateField()
+    confirmada = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"id:{self.id} area_comun:{self.area_comun} propietario:{self.propietario} fecha:{self.fecha}"
+
+    class Meta:
+        unique_together = [['area_comun', 'fecha']]
--- area_comun/urls.py
+++ area_comun/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import registrar_area_comun
+from .views import registrar_area_comun, lista_areas_comunes, reservar_area_comun, mis_reservas_area_comun
 
 urlpatterns = [
     path('registrar/', registrar_area_comun, name='registrar_area_comun'),
+    path('lista/', lista_areas_comunes, name='lista_areas_comunes'),
+    path('<int:area_comun_id>', reservar_area_comun, name='reservar_area_comun'),
+    path('misReservas/', mis_reservas_area_comun, name='mis_reservas_area_comun'),
 ]
