c24fb083d4c4d46bbfce9c8bf4314e7f5eb7884f
Ron <ronald.tucuman@outlook.com>
2022-12-27 18:59:00 -0400

    Feature/receipt (#77)
    * confirm and cancel part 1
    
    * base code for pdf receipts
    
    * receipt templated updated
    
    * receipt template updated
    
    * update to receipt template
    
    * confirm and cancel part 1
    
    * base code for pdf receipts
    
    * receipt templated updated
    
    * receipt template updated
    
    * update to receipt template
    
    * confirm reervation ready
    
    * installing receipt app
    
    * testing email
    
    * fixed email
    
    * debug email attachment
    
    * changes to send attachment
    
    * debug email process
    
    * debug email
    
    * debug email:
    
    * email with no attachments
    
    * layout changes in list reservations
    
    * cancel reservations
    
    * fix cancel email template
    
    * missing word:
    
    * missing word:

--- condomanager/email/SingleAzureEmailSender.py
+++ condomanager/email/SingleAzureEmailSender.py
@@ -1,6 +1,10 @@
 from azure.communication.email import EmailClient, EmailMessage, EmailContent, EmailAddress, EmailRecipients
+from azure.communication.email import EmailAttachment, EmailAttachmentType
 from django.conf import settings
 from django.core.mail import send_mail
+
+import base64
+import os
 
 
 class SingleAzureEmailSender:
@@ -31,3 +35,45 @@ class SingleAzureEmailSender:
             )
 
             client.send(message)
+
+    def send_message_attachment(self, subject, content_plain, content_html, mail_to, filepath):
+        if self.connection_string == '':
+            send_mail(
+                subject=subject,
+                message=content_plain,
+                from_email=self.email_sender,
+                recipient_list=[mail_to])
+        else:
+            client = EmailClient.from_connection_string(conn_str=self.connection_string)
+            content = EmailContent(
+                subject=subject,
+                plain_text=content_plain,
+                html=content_html
+            )
+
+            address = EmailAddress(email=mail_to)
+
+            if os.path.exists(filepath):
+                print('email pdf existe')
+                print(filepath)
+
+            with open(filepath, 'r') as file:
+                file_contents = file.read()
+            # print('email archivo pdf abierto')
+            file_bytes_64 = base64.b64encode(bytes(file_contents, 'utf-8'))
+            # print('convertido a base 64')
+            print(file_bytes_64)
+            attachment = EmailAttachment(
+                name='receipt.pdf',
+                attachment_type=EmailAttachmentType.PDF,
+                content_bytes_base64=file_bytes_64)
+            # print('objeto attachment creado')
+            message = EmailMessage(
+                sender=self.email_sender,
+                content=content,
+                recipients=EmailRecipients(to=[address]),
+                attachments=[attachment]
+            )
+            # print('objeto message creado')
+            client.send(message)
+            # print('email enviado')
---
+++ receipt/views.py
@@ -0,0 +1,176 @@
+import datetime
+
+from django.conf import settings
+from django.http import HttpResponse, FileResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string, get_template
+from django.utils.html import strip_tags
+from xhtml2pdf import pisa
+
+from condomanager.email.SingleAzureEmailSender import SingleAzureEmailSender
+from receipt.Reservation import Reservation
+from receipt.models import Receipt
+from area_comun.models import ReservaAreaComun
+
+import os
+import base64
+
+
+# Create your views here.
+
+@login_required
+def show_pdf(request, pdf_id=0):
+    if pdf_id <= 0:
+        return HttpResponse("El recibo no fue encontrado")
+    else:
+        filename = f'receipt_{pdf_id}.pdf'
+        path = os.path.join(settings.STATICFILES_DIRS[0], 'receipts/')
+        filepath = os.path.join(path, filename)
+        if os.path.exists(filepath):
+            return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+        else:
+            return HttpResponse("El recibo no fue encontrado")
+
+
+
+@login_required
+def list_reservations(request):
+    data = []
+    reservations = ReservaAreaComun.objects.all()
+    for rsv in reservations:
+        reservation = Reservation()
+
+        if Receipt.objects.filter(reservation_id=rsv.id).count() > 0:
+            receipt = Receipt.objects.filter(reservation_id=rsv.id).first()
+            reservation.is_canceled = receipt.is_canceled
+            reservation.receipt_id = receipt.id
+        else:
+            reservation.is_canceled = False
+            reservation.receipt_id = 0
+
+        reservation.reservation_id = rsv.id
+        reservation.is_confirmed = rsv.confirmada
+        reservation.user_id = rsv.propietario_id
+        reservation.username = rsv.propietario.username
+        reservation.common_area_id = rsv.area_comun_id
+        reservation.common_area_name = rsv.area_comun.nombre
+        reservation.begin_reservation_date = rsv.fecha
+        # reservation.end_reservation_date = rsv.fecha
+        reservation.amount = rsv.area_comun.costo
+        data.append(reservation)
+
+    return render(request, 'list_reservations.html', {'reservations': data})
+
+
+@login_required
+def confirm_reservation(request):
+    if request.method == 'POST':
+        try:
+            reservation_id = request.POST['reservation_id']
+            rsv = ReservaAreaComun.objects.filter(id=reservation_id).first()
+            rsv.confirmada = True
+            rsv.save()
+            receipt = Receipt()
+            receipt.reservation_id = rsv.id
+            receipt.registered_date = datetime.datetime.now()
+            receipt.amount = rsv.area_comun.costo
+            receipt.user_id = rsv.propietario_id
+            receipt.common_area_id = rsv.area_comun_id
+            if Receipt.objects.count() == 0:
+                receipt.receipt_number = 1
+            else:
+                receipt.receipt_number = Receipt.objects.latest('receipt_number').receipt_number + 1
+            receipt.save()
+
+            file_path = generate_pdf(request, receipt)
+            send_email(receipt.user, 'confirm_reservation', receipt, file_path)
+            return HttpResponse(f"La reserva del area comun ha sido confirmada")
+        except:
+            return HttpResponse("No se pudo confirmar reserva. Intentelo nuevamente.")
+
+    else:
+        return HttpResponse("No se pudo confirmar reserva. Intentelo nuevamente.")
+
+
+@login_required
+def cancel_reservation(request):
+    if request.method == 'POST':
+        try:
+            reservation_id = request.POST['reservation_id']
+            rsv = ReservaAreaComun.objects.filter(id=reservation_id).first()
+            rsv.confirmada = False
+            rsv.save()
+
+            receipt = Receipt.objects.filter(reservation_id=reservation_id).first()
+            receipt.is_canceled = True
+            receipt.save()
+
+            send_email(receipt.user, 'cancel_reservation', receipt)
+            return HttpResponse(f"La reserva del area comun ha sido cancelada")
+        except:
+            return HttpResponse("No se pudo cancelar reserva. Intentelo nuevamente.")
+
+    else:
+        return HttpResponse("No se pudo cancelar reserva. Intentelo nuevamente.")
+
+
+def send_email(user, command, receipt, filepath=''):
+    subject = ''
+    template_name = ''
+
+    if command == 'cancel_reservation':
+        subject = 'Reserva de Area Comun Cancelada'
+        template_name = 'email/cancel_reservation.html'
+    elif command == 'confirm_reservation':
+        subject = 'Reserva de Area Comun Confirmada'
+        template_name = 'email/confirm_reservation.html'
+
+    if subject != '':
+        html_message = render_to_string(template_name,
+                                        {'user': user, 'receipt': receipt})
+        plain_message = strip_tags(html_message)
+        mail_to = user.email
+        email_sender = SingleAzureEmailSender()
+        # print('path del archivo pdf')
+        # print(filepath)
+        # if os.path.exists(filepath):
+        #     print('existe el archivo pdf')
+        #     email_sender.send_message_attachment(
+        #         subject=subject,
+        #         content_plain=plain_message,
+        #         content_html=html_message,
+        #         mail_to=mail_to,
+        #         filepath=filepath)
+        # else:
+        #     print('no existe el archivo pdf')
+        email_sender.send_message(
+                subject=subject,
+                content_plain=plain_message,
+                content_html=html_message,
+                mail_to=mail_to)
+
+
+def generate_pdf(request, receipt):
+    filename = f'receipt_{receipt.reservation_id}.pdf'
+    path = os.path.join(settings.STATICFILES_DIRS[0], 'receipts/')
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    filepath = os.path.join(path, filename)
+    dest = open(filepath, 'w+b')
+    template = get_template('receipts/pdf_template1.html')
+    context = {
+        'data': receipt,
+        'user': request.user
+    }
+    html = template.render(context)
+    result = pisa.CreatePDF(
+        html,
+        dest=dest,
+    )
+
+    dest.close()
+
+    return filepath
