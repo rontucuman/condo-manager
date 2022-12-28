4cdd75bd41600bff16777af8d3780ec30c98fa5f
rontucuman <ronald.tucuman@outlook.com>
2022-12-28 16:23:18 -0400

    changes to receipt model

--- receipt/Reservation.py
+++ receipt/Reservation.py
@@ -5,14 +5,15 @@ class Reservation:
     def __init__(self):
         self.reservation_id = 0
         self.receipt_id = 0
+        self.common_area_id = 0
         self.user_id = 0
         self.username = ''
-        self.common_area_id = 0
         self.common_area_name = ''
-        self.reservation_date = datetime.datetime.now()
+        # self.reservation_date = datetime.datetime.now()
         self.begin_reservation_date = datetime.datetime.now()
         self.end_reservation_date = datetime.datetime.now()
         self.amount = 0
         self.is_confirmed = False
         self.is_canceled = False
+        self.receipt_filename = ''
 
--- receipt/views.py
+++ receipt/views.py
@@ -15,23 +15,20 @@ from area_comun.models import ReservaAreaComun
 
 import os
 import base64
+import uuid
 
 
 # Create your views here.
 
 @login_required
-def show_pdf(request, pdf_id=0):
-    if pdf_id <= 0:
-        return HttpResponse("El recibo no fue encontrado")
-    else:
-        filename = f'receipt_{pdf_id}.pdf'
+def show_pdf(request, doc='none'):
+    if settings.ENVIRONMENT == 'local':
         path = os.path.join(settings.STATICFILES_DIRS[0], 'receipts/')
-        filepath = os.path.join(path, filename)
+        filepath = os.path.join(path, doc)
         if os.path.exists(filepath):
-            return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
-        else:
-            return HttpResponse("El recibo no fue encontrado")
+            return FileResponse(open(filepath, 'rb'), content_type='application/pdf', filename=doc)
 
+    return HttpResponse("El recibo no fue encontrado")
 
 
 @login_required
@@ -43,21 +40,33 @@ def list_reservations(request):
 
         if Receipt.objects.filter(reservation_id=rsv.id).count() > 0:
             receipt = Receipt.objects.filter(reservation_id=rsv.id).first()
+            reservation.reservation_id = receipt.reservation_id
             reservation.is_canceled = receipt.is_canceled
             reservation.receipt_id = receipt.id
+            reservation.is_confirmed = receipt.is_reservation_confirmed
+            reservation.user_id = receipt.user_id
+            reservation.username = receipt.user.username
+            reservation.common_area_id = receipt.common_area_id
+            reservation.common_area_name = receipt.common_area.nombre
+            reservation.begin_reservation_date = receipt.begin_reservation_date.date()
+            reservation.end_reservation_date = receipt.end_reservation_date
+            reservation.amount = receipt.reservation_amount
+            reservation.receipt_filename = receipt.filename
+
         else:
+            reservation.reservation_id = rsv.id
             reservation.is_canceled = False
             reservation.receipt_id = 0
+            reservation.is_confirmed = rsv.confirmada
+            reservation.user_id = rsv.propietario_id
+            reservation.username = rsv.propietario.username
+            reservation.common_area_id = rsv.area_comun_id
+            reservation.common_area_name = rsv.area_comun.nombre
+            reservation.begin_reservation_date = rsv.fecha
+            reservation.end_reservation_date = rsv.fecha
+            reservation.amount = '{:.2f}'.format(rsv.area_comun.costo)
+            reservation.receipt_filename = 'not found'
 
-        reservation.reservation_id = rsv.id
-        reservation.is_confirmed = rsv.confirmada
-        reservation.user_id = rsv.propietario_id
-        reservation.username = rsv.propietario.username
-        reservation.common_area_id = rsv.area_comun_id
-        reservation.common_area_name = rsv.area_comun.nombre
-        reservation.begin_reservation_date = rsv.fecha
-        # reservation.end_reservation_date = rsv.fecha
-        reservation.amount = rsv.area_comun.costo
         data.append(reservation)
 
     return render(request, 'list_reservations.html', {'reservations': data})
@@ -73,19 +82,26 @@ def confirm_reservation(request):
             rsv.save()
             receipt = Receipt()
             receipt.reservation_id = rsv.id
-            receipt.registered_date = datetime.datetime.now()
-            receipt.amount = rsv.area_comun.costo
             receipt.user_id = rsv.propietario_id
             receipt.common_area_id = rsv.area_comun_id
+
             if Receipt.objects.count() == 0:
                 receipt.receipt_number = 1
             else:
                 receipt.receipt_number = Receipt.objects.latest('receipt_number').receipt_number + 1
-            receipt.save()
 
-            file_path = generate_pdf(request, receipt)
-            send_email(receipt.user, 'confirm_reservation', receipt, file_path)
-            return HttpResponse(f"La reserva del area comun ha sido confirmada")
+            receipt.registered_date = datetime.datetime.now()
+            receipt.is_canceled = False
+            receipt.is_reservation_confirmed = True
+            receipt.begin_reservation_date = rsv.fecha
+            receipt.is_reservation_canceled = False
+            receipt.reservation_amount = rsv.area_comun.costo
+            receipt.paid_amount = rsv.area_comun.costo
+            filename = generate_pdf(request, receipt)
+            receipt.filename = filename
+            receipt.save()
+            send_email('confirm_reservation', receipt, filename)
+            return HttpResponse(filename)
         except:
             return HttpResponse("No se pudo confirmar reserva. Intentelo nuevamente.")
 
@@ -115,7 +131,7 @@ def cancel_reservation(request):
         return HttpResponse("No se pudo cancelar reserva. Intentelo nuevamente.")
 
 
-def send_email(user, command, receipt, filepath=''):
+def send_email(command, receipt, filename=''):
     subject = ''
     template_name = ''
 
@@ -128,9 +144,9 @@ def send_email(user, command, receipt, filepath=''):
 
     if subject != '':
         html_message = render_to_string(template_name,
-                                        {'user': user, 'receipt': receipt})
+                                        {'user': receipt.user, 'receipt': receipt})
         plain_message = strip_tags(html_message)
-        mail_to = user.email
+        mail_to = receipt.user.email
         email_sender = SingleAzureEmailSender()
         # print('path del archivo pdf')
         # print(filepath)
@@ -152,7 +168,7 @@ def send_email(user, command, receipt, filepath=''):
 
 
 def generate_pdf(request, receipt):
-    filename = f'receipt_{receipt.reservation_id}.pdf'
+    filename = f'receipt_{uuid.uuid4()}.pdf'
     path = os.path.join(settings.STATICFILES_DIRS[0], 'receipts/')
 
     if not os.path.exists(path):
@@ -173,4 +189,4 @@ def generate_pdf(request, receipt):
 
     dest.close()
 
-    return filepath
+    return filename
