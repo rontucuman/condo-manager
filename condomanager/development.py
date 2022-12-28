47357e3a7b1280466d7a956dda2a021649307448
Ron <ronald.tucuman@outlook.com>
2022-12-28 18:58:56 -0400

    Feature/receipt (#78)
    * confirm and cancel part 1
    
    * base code for pdf receipts
    
    * receipt templated updated
    
    * receipt template updated
    
    * update to receipt template
    
    * confirm and cancel part 1
    
    * base code for pdf receipts
    
    * receipt templated updated
    
    * receipt template updated
    
    * update to receipt template
    
    * confirm reervation ready
    
    * installing receipt app
    
    * testing email
    
    * fixed email
    
    * debug email attachment
    
    * changes to send attachment
    
    * debug email process
    
    * debug email
    
    * debug email:
    
    * email with no attachments
    
    * layout changes in list reservations
    
    * cancel reservations
    
    * fix cancel email template
    
    * missing word:
    
    * missing word:
    
    * changes to receipt model
    
    * updating gitignore
    
    * missing static files dir
    
    * missing static files dir
    
    * missing static files dir
    
    * Revert "missing static files dir"
    
    This reverts commit 1d9472408fe8ca3b85bd62419febffc7727bb281.
    
    * fixed error found in console.log

--- condomanager/development.py
+++ condomanager/development.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+
+ENVIRONMENT = 'production'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,7 +134,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
@@ -143,7 +146,7 @@ CORS_ALLOWED_ORIGINS = [DOMAIN_URL]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "condomanager/static",
+    Path.joinpath(BASE_DIR, 'condomanager/static')
 ]
 
 LOGIN_REDIRECT_URL = 'dashboard'
--- receipt/models.py
+++ receipt/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from area_comun.models import AreaComun, ReservaAreaComun
 from django.conf import settings
 from django.db import models
@@ -11,9 +13,19 @@ YESNO_CHOICES = (
 
 class Receipt(models.Model):
     receipt_number = models.PositiveIntegerField(verbose_name='Recibo No.', unique=True)
-    registered_date = models.DateTimeField(verbose_name='Fecha')
-    amount = models.DecimalField(verbose_name='Monto', decimal_places=2, max_digits=10)
+    registered_date = models.DateTimeField(verbose_name='Fecha de Pago')
+    reservation_amount = models.DecimalField(verbose_name='Monto de Reserva', decimal_places=2, max_digits=10,
+                                             default=0)
+    paid_amount = models.DecimalField(verbose_name='Monto Pagado', decimal_places=2, max_digits=10, default=0)
     is_canceled = models.BooleanField(verbose_name='Cancelado?', choices=YESNO_CHOICES, default=False)
+    begin_reservation_date = models.DateTimeField(verbose_name='Inicio de Reserva', blank=True, null=True)
+    end_reservation_date = models.DateTimeField(verbose_name='Fin de Reserva', blank=True, null=True)
+    is_reservation_confirmed = models.BooleanField(verbose_name='Reserva Confirmada?', choices=YESNO_CHOICES,
+                                                   default=False)
+    is_reservation_canceled = models.BooleanField(verbose_name='Reserva Cancelada?', choices=YESNO_CHOICES,
+                                                  default=False)
+    # filepath = models.FilePathField(max_length=500, verbose_name='Ubicacion del Recibo', blank=True, null=True)
+    filename = models.CharField(max_length=48, blank=True, null=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
