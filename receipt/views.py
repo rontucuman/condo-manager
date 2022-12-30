99b495cea2fa7e12456471e13bfb33d648d3fbca
rontucuman <ronald.tucuman@outlook.com>
2022-12-30 03:42:53 -0400

    implementing azure blob storage for saving receipts

--- receipt/views.py
+++ receipt/views.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, FileResponse
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string, get_template
 from django.utils.html import strip_tags
 from xhtml2pdf import pisa
