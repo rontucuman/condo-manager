b6f6a652df87894394df6c71703b1aea01f2e419
rontucuman <ronald.tucuman@outlook.com>
2023-01-02 13:32:13 -0400

    reservations view updated and filters added

---
+++ receipt/CustomFilters.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+class CustomFilters:
+    filter_username: str
+    filter_common_area_name: str
+    filter_begin_reservation_date: datetime.date
+    filter_status: str
+
+    def __init__(self):
+        self.filter_username = ''
+        self.filter_common_area_name = ''
+        self.filter_begin_reservation_date = ''
+        self.filter_status = ''
--- receipt/urls.py
+++ receipt/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('list-reservations/', views.list_reservations, name='list_reservations'),
+    # path('list-reservations/', views.list_reservations, name='list_reservations'),
+    # path('list-reservations1/', views.list_reservations1, name='list_reservations1'),
+    path('list-reservations2/', views.list_reservations2, name='list_reservations2'),
+    path('get-reservations-content/', views.get_reservations_content, name='get_reservations_content'),
+    path('get-pagination-content/', views.get_pagination_content, name='get_pagination_content'),
+    # path('list-reservations2/<str:page>/<str:numitems>', views.list_reservations2, name='list_reservations2'),
     path('process-confirm-reservation/', views.confirm_reservation, name='confirm_reservation'),
     path('process-cancel-reservation/', views.cancel_reservation, name='cancel_reservation'),
     path('show-pdf/<str:doc>', views.show_pdf, name='show_pdf')
