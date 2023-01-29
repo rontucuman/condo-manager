56c4060228f4a1701b871962d328e5e86e8d1f42
Ron <ronald.tucuman@outlook.com>
2023-01-29 02:08:12 -0400

    Feature/receipt part 03 (#83)
    * changed icon
    
    * added range filtering for date
    
    * fix
    
    * model update

--- receipt/CustomFilters.py
+++ receipt/CustomFilters.py
@@ -5,10 +5,12 @@ class CustomFilters:
     filter_username: str
     filter_common_area_name: str
     filter_begin_reservation_date: datetime.date
+    filter_end_reservation_date: datetime.date
     filter_status: str
 
     def __init__(self):
         self.filter_username = ''
         self.filter_common_area_name = ''
         self.filter_begin_reservation_date = ''
+        self.filter_end_reservation_date = ''
         self.filter_status = ''
